d37ab63e8ea295b6615cd9a0a01ba0d3d6b6e61e
vkostetskiy135 <vkostetskiy135@gmail.com>
2024-05-21 14:25:55 +0300

    HW 10_27 for Module 5

--- Module5/HW2023_10_27.py
+++ Module5/HW2023_10_27.py
@@ -2,11 +2,13 @@ class House:
     def __init__(self):
         self.numberOfFloors = 0
 
+    def __str__(self):
+        return f'''Number of floors -> {self.numberOfFloors}'''
     def setNewNumberOfFloors(self, floors):
         self.numberOfFloors = floors
-        print(self.numberOfFloors)
 
 
 house = House()
-print(house.numberOfFloors)
+print(house)
 house.setNewNumberOfFloors(5)
+print(house)
