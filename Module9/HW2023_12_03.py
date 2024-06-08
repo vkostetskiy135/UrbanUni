b437147261078b4fcae9366356b960d74288fa36
vkostetskiy135 <vkostetskiy135@gmail.com>
2024-06-08 18:17:35 +0300

    Added HW2023_12_03.py

---
+++ Module9/HW2023_12_03.py
@@ -0,0 +1,22 @@
+def decor(func):
+    def wrapper(*args, **kwargs):
+        func_result = func(*args, **kwargs)
+        dividable_by = []
+        for i in range(2, func_result):
+            if func_result % i == 0:
+                dividable_by.append(i)
+        if not dividable_by:
+            print('Простое')
+        else:
+            print('Сложное')
+        return func_result
+    return wrapper
+
+
+@decor
+def sum_three(a, b, c):
+    return a + b + c
+
+print(sum_three(2, 3, 6))
+
+print(sum_three(4, 5, 6))
