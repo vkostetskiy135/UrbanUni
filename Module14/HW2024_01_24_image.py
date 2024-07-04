571de9d8d0102fba5d7b8861eb66fb2ab49c91ab
vkost <vkostetskiy135@gmail.com>
2024-07-04 15:03:21 +0300

    Added HW2024_01_24_image.py for module 14

--- Module14/HW2024_01_24_image.py
+++ Module14/HW2024_01_24_image.py
@@ -1,25 +1,41 @@
-import requests
+# def extract_image_links(html_text):
+#     i_start = None
+#     i_end = None
+#     result = []
+#     for i, l in enumerate(html_text):
+#         if l in ['\'', '"'] and not i_start:
+#             i_start = i + 1
+#         elif l in ['\'', '"']:
+#             i_end = i
+#             result.append(html_text[i_start:i_end])
+#             i_start = None
+#     return result
+#
+#
+#
+#
+# sample_html = "<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> <img src='https://example.com/image3.gif'>"
+# image_links = extract_image_links(sample_html)
+# if image_links:
+#   for image_link in image_links:
+#     print(image_link)
+# else:
+#   print("Нет ссылок с картинками в HTML тексте.")
+
+import re
+
 
 def extract_image_links(html_text):
-    i_start = None
-    i_end = None
-    result = []
-    for i, l in enumerate(html_text):
-        if l == '\'' and not i_start:
-            i_start = i + 1
-        elif l == '\'':
-            i_end = i
-            result.append(html_text[i_start:i_end])
-            i_start = None
-    return result
-
-
+    pattern = r'https?://[^\s]+?\.(jpg|jpeg|png|gif)'
+    image_links = re.findall(pattern, html_text)
+    return image_links
 
 
 sample_html = "<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> <img src='https://example.com/image3.gif'>"
+
 image_links = extract_image_links(sample_html)
 if image_links:
-  for image_link in image_links:
-    print(image_link)
+    for image_link in image_links:
+        print(image_link)
 else:
-  print("Нет ссылок с картинками в HTML тексте.")
+    print("Нет ссылок с картинками в HTML тексте.")
