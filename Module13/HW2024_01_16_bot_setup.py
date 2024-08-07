882aa78a74d04bcc29566854fd779ea63f8556d6
vkost <vkostetskiy135@gmail.com>
2024-08-07 21:35:02 +0300

    Added HW2024_01_16_bot_setup.py.py for module 13.

---
+++ Module13/HW2024_01_16_bot_setup.py
@@ -0,0 +1,18 @@
+from aiogram import executor, Dispatcher, types, Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from bot_api_key import token
+bot = Bot(token=token)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
