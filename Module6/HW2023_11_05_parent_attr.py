0a6789d8c0dbbeb8b536cb74e7b547d3434e2134
vkost <vkostetskiy135@gmail.com>
2024-08-09 21:10:47 +0300

    Added HW2023_11_05_parent_attr.py for module 6.

--- Module6/HW2023_11_05_parent_attr.py
+++ Module6/HW2023_11_05_parent_attr.py
@@ -42,13 +42,10 @@ class Sedan(Vehicle):
 
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
-# Изначальные свойства
 vehicle1.print_info()
 
-# Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
 
-# Проверяем что поменялось
 vehicle1.print_info()
